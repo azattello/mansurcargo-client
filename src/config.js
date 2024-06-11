957fc9f1ad0d397a2eba28f7c0c50f966f1f04e1
Abdykali Azat <azat.abdykalli@gmail.com>
2024-06-11 20:11:45 +0500

    mansur clientcommit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://mansurcargo.kz:3001',
+    
+    nameCargo: 'Mansur Cargo'
+  };
+  
+  
+export default config;
+  
